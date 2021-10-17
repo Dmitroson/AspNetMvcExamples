3455ea20a2cb571db3a5202141a27d694fb303f7
Sasha <sanyokkotenko2002@gmail.com>
2021-10-17 12:43:17 +0300

    Add project files.

---
+++ MvcTutorial/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using MvcTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult StudentDetails()
+        {
+            var student = new Student();
+            student.Name = "Petro";
+            student.Age = 25;
+
+            return View(student);
+        }
+
+        [HttpGet]
+        public ActionResult CreateStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateStudent(Student student)
+        {
+            var list = new List<Student>();
+            if (ModelState.IsValid)
+            {
+                list.Add(student);
+                return RedirectToAction(nameof(StudentDetails));
+            }
+            return View(student);
+        }
+    }
+}
---
+++ MvcTutorial/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTutorial.Models
+{
+    public class Student
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        public int Age { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
